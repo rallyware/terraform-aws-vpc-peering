6b8e77de7be6be533e94d13cbfb3aa0da5e8067e
Vladimir <26582191+SweetOps@users.noreply.github.com>
2021-09-22 16:29:16 +0300

    feat: initial implementation (#1)

---
+++ accepter.tf
@@ -0,0 +1,88 @@
+locals {
+  accepter_subnet_ids                    = length(var.accepter_subnet_ids) > 0 ? sort(var.accepter_subnet_ids) : try(distinct(sort(flatten(data.aws_subnet_ids.accepter[*].ids))))
+  accepter_subnet_ids_count              = var.accepter_subnets_count != null ? var.accepter_subnets_count : length(local.accepter_subnet_ids)
+  accepter_vpc_id                        = one(data.aws_vpc.accepter[*].id)
+  accepter_account_id                    = one(data.aws_caller_identity.accepter[*].account_id)
+  accepter_region                        = one(data.aws_region.accepter[*].name)
+  accepter_route_table_ids               = length(var.accepter_route_table_ids) > 0 ? sort(var.accepter_route_table_ids) : try(distinct(sort(data.aws_route_table.accepter[*].route_table_id)), [])
+  accepter_route_table_ids_count         = var.accepter_route_tables_count != null ? var.accepter_route_tables_count : length(local.accepter_route_table_ids)
+  accepter_cidr_block_associations       = length(var.accepter_vpc_cidr_blocks) > 0 ? sort(var.accepter_vpc_cidr_blocks) : flatten(data.aws_vpc.accepter[*].cidr_block_associations[*].cidr_block)
+  accepter_cidr_block_associations_count = length(local.accepter_cidr_block_associations)
+}
+
+module "accepter" {
+  source  = "cloudposse/label/null"
+  version = "0.24.1"
+
+  attributes = var.add_attribute_tag ? ["accepter"] : []
+  tags       = var.add_attribute_tag ? { "Side" = "accepter" } : {}
+  context    = module.this.context
+}
+
+data "aws_caller_identity" "accepter" {
+  count    = local.count
+  provider = aws.accepter
+}
+
+data "aws_region" "accepter" {
+  count    = local.count
+  provider = aws.accepter
+}
+
+# Lookup accepter's VPC so that we can reference the CIDR
+data "aws_vpc" "accepter" {
+  count    = local.count
+  provider = aws.accepter
+  id       = var.accepter_vpc_id
+  tags     = var.accepter_vpc_tags
+}
+
+# Lookup accepter subnets
+data "aws_subnet_ids" "accepter" {
+  count    = local.count
+  provider = aws.accepter
+
+  vpc_id = local.accepter_vpc_id
+  tags   = var.accepter_subnet_tags
+}
+
+# Lookup accepter route tables
+data "aws_route_table" "accepter" {
+  count     = module.this.enabled ? local.accepter_subnet_ids_count : 0
+  provider  = aws.accepter
+  subnet_id = element(local.accepter_subnet_ids, count.index)
+}
+
+# Create routes from accepter to requester
+resource "aws_route" "accepter" {
+  count                     = module.this.enabled ? local.accepter_route_table_ids_count * local.requester_cidr_block_associations_count : 0
+  provider                  = aws.accepter
+  route_table_id            = local.accepter_route_table_ids[floor(count.index / local.requester_cidr_block_associations_count)]
+  destination_cidr_block    = local.requester_cidr_block_associations[count.index % local.requester_cidr_block_associations_count]
+  vpc_peering_connection_id = join("", aws_vpc_peering_connection.requester[*].id)
+
+  depends_on = [
+    data.aws_route_table.accepter,
+    aws_vpc_peering_connection_accepter.accepter,
+    aws_vpc_peering_connection.requester,
+  ]
+}
+
+# Accepter's side of the connection.
+resource "aws_vpc_peering_connection_accepter" "accepter" {
+  count                     = local.count
+  provider                  = aws.accepter
+  vpc_peering_connection_id = join("", aws_vpc_peering_connection.requester[*].id)
+  auto_accept               = var.auto_accept
+  tags                      = module.accepter.tags
+}
+
+resource "aws_vpc_peering_connection_options" "accepter" {
+  count                     = local.count
+  provider                  = aws.accepter
+  vpc_peering_connection_id = local.active_vpc_peering_connection_id
+
+  accepter {
+    allow_remote_vpc_dns_resolution = var.accepter_allow_remote_vpc_dns_resolution
+  }
+}
---
+++ requester.tf
@@ -0,0 +1,95 @@
+locals {
+  requester_subnet_ids                    = length(var.requester_subnet_ids) > 0 ? sort(var.requester_subnet_ids) : try(distinct(sort(flatten(data.aws_subnet_ids.requester[*].ids))), [])
+  requester_subnet_ids_count              = var.requester_subnets_count != null ? var.requester_subnets_count : length(local.requester_subnet_ids)
+  requester_vpc_id                        = one(data.aws_vpc.requester[*].id)
+  requester_route_table_ids               = length(var.requester_route_table_ids) > 0 ? sort(var.requester_route_table_ids) : try(distinct(sort(data.aws_route_table.requester.*.route_table_id)), [])
+  requester_route_table_ids_count         = var.requester_route_tables_count != null ? var.requester_route_tables_count : length(local.requester_route_table_ids)
+  requester_cidr_block_associations       = flatten(data.aws_vpc.requester[*].cidr_block_associations[*].cidr_block)
+  requester_cidr_block_associations_count = length(local.requester_cidr_block_associations)
+
+  # Options can't be set until the connection has been accepted and is active,
+  # so create an explicit dependency on the accepter when setting options.
+  active_vpc_peering_connection_id = one(aws_vpc_peering_connection_accepter.accepter[*].id)
+}
+
+module "requester" {
+  source  = "cloudposse/label/null"
+  version = "0.24.1"
+
+  attributes = var.add_attribute_tag ? ["requester"] : []
+  tags       = var.add_attribute_tag ? { "Side" = "requester" } : {}
+  context    = module.this.context
+}
+
+data "aws_caller_identity" "requester" {
+  count    = local.count
+  provider = aws.requester
+}
+
+data "aws_region" "requester" {
+  count    = local.count
+  provider = aws.requester
+}
+
+# Lookup requester VPC so that we can reference the CIDR
+data "aws_vpc" "requester" {
+  count    = local.count
+  provider = aws.requester
+  id       = var.requester_vpc_id
+  tags     = var.requester_vpc_tags
+}
+
+# Lookup requester subnets
+data "aws_subnet_ids" "requester" {
+  count    = local.count
+  provider = aws.requester
+  vpc_id   = local.requester_vpc_id
+  tags     = var.requester_subnet_tags
+}
+
+# Lookup requester route tables
+data "aws_route_table" "requester" {
+  count     = module.this.enabled ? local.requester_subnet_ids_count : 0
+  provider  = aws.requester
+  subnet_id = element(local.requester_subnet_ids, count.index)
+}
+
+resource "aws_vpc_peering_connection" "requester" {
+  count         = local.count
+  provider      = aws.requester
+  vpc_id        = local.requester_vpc_id
+  peer_vpc_id   = local.accepter_vpc_id
+  peer_owner_id = local.accepter_account_id
+  peer_region   = local.accepter_region
+  auto_accept   = false
+
+  tags = module.requester.tags
+}
+
+resource "aws_vpc_peering_connection_options" "requester" {
+  count    = local.count
+  provider = aws.requester
+
+  # As options can't be set until the connection has been accepted
+  # create an explicit dependency on the accepter.
+  vpc_peering_connection_id = local.active_vpc_peering_connection_id
+
+  requester {
+    allow_remote_vpc_dns_resolution = var.requester_allow_remote_vpc_dns_resolution
+  }
+}
+
+# Create routes from requester to accepter
+resource "aws_route" "requester" {
+  count                     = module.this.enabled ? local.requester_route_table_ids_count * local.accepter_cidr_block_associations_count : 0
+  provider                  = aws.requester
+  route_table_id            = local.requester_route_table_ids[floor(count.index / local.accepter_cidr_block_associations_count)]
+  destination_cidr_block    = local.accepter_cidr_block_associations[count.index % local.accepter_cidr_block_associations_count]
+  vpc_peering_connection_id = one(aws_vpc_peering_connection.requester[*].id)
+
+  depends_on = [
+    data.aws_route_table.requester,
+    aws_vpc_peering_connection.requester,
+    aws_vpc_peering_connection_accepter.accepter,
+  ]
+}
